62583274f5670bc92720bc8dec45769109ded65c
Victor <ivchenko.victor.04@gmail.com>
2022-03-28 01:02:58 +0300

    fix bug

--- download.js
+++ download.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
-
-require("vdocs-vue").default({
+const { default: parse, IGNORE_FILES } = require("vdocs-vue");
+IGNORE_FILES.push(/package\-lock/);
+parse({
   owner: process.env.VUE_APP_GH_OWNER,
   branch: process.env.VUE_APP_GH_BRANCH ?? "main",
   repository: process.env.VUE_APP_GH_REPO,
