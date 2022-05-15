56666f3ccdefb123fb1438ae6ad688643305f3e6
lentryd <lentryd@users.noreply.github.com>
2022-05-15 18:50:43 +0000

    update content

---
+++ precache-manifest.0ffcbc7558a9f8c50c062828a75b886b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f27cdcf6cd38ba896b",
+    "url": "/VDocs/css/app.01321a59.css"
+  },
+  {
+    "revision": "359385c200776dcf7704",
+    "url": "/VDocs/css/chunk-0dfefd9e.14f87756.css"
+  },
+  {
+    "revision": "6ed510ee80cb15f1a340",
+    "url": "/VDocs/css/chunk-249d13de.7e2f84ba.css"
+  },
+  {
+    "revision": "06dfafbc2e3bc6cb95ea",
+    "url": "/VDocs/css/chunk-4c2b1572.3e565104.css"
+  },
+  {
+    "revision": "a5879dd44970a54c6dd8151e03d8b7b0",
+    "url": "/VDocs/img/android-chrome-192x192.a5879dd4.png"
+  },
+  {
+    "revision": "c490071b4bcfac763dc300dd858daa8e",
+    "url": "/VDocs/img/android-chrome-512x512.c490071b.png"
+  },
+  {
+    "revision": "c3a50d0b49533e063ffe54d2d9bb73fe",
+    "url": "/VDocs/img/apple-touch-icon.c3a50d0b.png"
+  },
+  {
+    "revision": "c8940f4a6946af0234243c2b234e3814",
+    "url": "/VDocs/index.html"
+  },
+  {
+    "revision": "e6f27cdcf6cd38ba896b",
+    "url": "/VDocs/js/app.4aeebf38.js"
+  },
+  {
+    "revision": "359385c200776dcf7704",
+    "url": "/VDocs/js/chunk-0dfefd9e.747c9b2d.js"
+  },
+  {
+    "revision": "6ed510ee80cb15f1a340",
+    "url": "/VDocs/js/chunk-249d13de.fa742dcf.js"
+  },
+  {
+    "revision": "06dfafbc2e3bc6cb95ea",
+    "url": "/VDocs/js/chunk-4c2b1572.7f39cd1c.js"
+  },
+  {
+    "revision": "65d16f862dc0fb62ebb0",
+    "url": "/VDocs/js/chunk-vendors.f79c073b.js"
+  },
+  {
+    "revision": "4deaaffe054161f589c05b15f338de24",
+    "url": "/VDocs/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/VDocs/robots.txt"
+  }
+]);
