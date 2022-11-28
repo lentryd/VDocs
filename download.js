4ee59de68992937f455ca0062f374e8bb3f68224
Victor <ivchenko.victor.04@gmail.com>
2022-11-28 12:50:33 +0300

    игнорирование файлов ico (возможно исправлю)

--- download.js
+++ download.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { default: parse, IGNORE_FILES } = require("vdocs-vue");
-IGNORE_FILES.push(/package\-lock/);
+IGNORE_FILES.push(/package\-lock/, /\.ico$/);
 parse({
   owner: process.env.VUE_APP_GH_OWNER,
   branch: process.env.VUE_APP_GH_BRANCH ?? "main",
